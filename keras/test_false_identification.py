026e2936f0cd945ea3fba133966713c068ad27cd
Fiachra <fiachra@gamma2021.dunsink.dias.ie>
2022-01-28 19:27:05 +0000

    Revert "Added new scripts, changed testing predictions to create a histogram distribution of predictions made, tried to make another version of the testing predcitions script that would load the images as a tensorflow dataset so that it would work quicker and it does but the predictions are differenet than the predictions made by loading and analysing each image singly. Perhaps I made a mistake n my code, perhaps I need to remove the label axis from the tensorflow dataset before using the predict model"
    This reverts commit 367b07ddaeb18a6bc99ff24b855d359499ae3ad8.

--- keras/test_false_identification.py
+++ keras/test_false_identification.py
@@ -1,9 +1,8 @@
+import PIL
+from PIL import image
 import os
 import csv
-import numpy as np
-from matplotlib import pyplot as plt
-import time
-from os.path import dirname
+
 #ensures that tensorflow is used as the backend
 import tensorflow as tf
 #tf.compact.v1.keras.backend.set_session()
@@ -18,97 +17,34 @@ from tensorflow import keras
 from tensorflow.keras import layers
 from tensorflow.keras import models
 
-from tensorflow.keras.preprocessing.image import load_img, img_to_array
-from tensorflow.keras.preprocessing import image_dataset_from_directory
+
 model_name = input("enter the full path of model: ")
-CNN_model = models.load_model(model_name)
+CCN_model = models.load_model(model_name)
 
-initial_dir = os.getcwd()
 
-folder_input = input("folder containing folders labelled 0 and 1 respectively that contain images that you want to test false identification: ")
-start_time = time.perf_counter()
+
+folder_input = input("folder containing images that you want to test false identification: ")
+
 #need to check which is confirmed and which is negative 0 or 1
-#initialise list of predictions, elements are tuples (Label, prediction, false_pred, file_name)
-#Label = 1 for Confirmed files, 0 for Rejected files, prediction = prediction value from neural network has value between 0 and 1, false_pred = Boolean true or false if the prediction is < 55% for Confirmed images or >45% for Rejected images it is considered false, file_name is the name of the .png file
-# Confidence of less than 55% from prediction is considered false as it may be interesting to see which predictions the network is very unsure about
-pred_list = []
+#if ConfirmedFiles are labelled 1 then any predictions of 0 by the network by the files in this folder will be false negatives
+if folder_input[-1] == 1:
+    false_pos_or_neg = "false negative"
+    #if RejectedFiles are labelled 0 then any predictions of 1 by the network by the files in this folder will be false positives
+elif folder_input[-1] == 0:
+    false_pos_or_neg = "false positive"
+else:
+    print("folder is set up wrong")
+file_list = os.listdir(folder_input)
 
-def normalise(image, label):
-    image = tf.cast(image/255., tf.float32)
-    return image, label
+false_list = []
+for item in file_list:
+    prediction = model_name.predict(item)
 
-#make a tf.dataset generator list
-data_dir = folder_input
-file_dataset = image_dataset_from_directory(
-    data_dir,
-    labels="inferred",
-    color_mode = "grayscale",
-    batch_size=32,
-    image_size = (128,128),
-    interpolation = "bilinear")
-image_paths = file_dataset.file_paths
-labels_list = []
-for x, y in file_dataset:
-    labels_list.append(y)
-file_dataset = file_dataset.map(normalise)
+    if prediction != folder_input[-1]:
+        false_list.append(item)
 
-prediction_list = CNN_model.predict(file_dataset)
-#iterate over all the files, first in the rejected files folder (0) and then in the Confirmed Files folder (1)
-
-#for i in range(2):
-    #os.chdir(folder_input + "/" + str(i))
-    #print(f"cwd: {os.getcwd()}")
-    #file_list = os.listdir(folder_input + "/" + str(i))
-    
-    #print(f"file_list: {file_list}")
-#    for index, item in enumerate(file_list):
-        #load image and convert to numpy array
-#        pil_image = load_img(item, color_mode = "grayscale", target_size = (128, 128), interpolation = "bilinear")
-#        image_np_array = img_to_array(pil_image)
-#        image_rescale = image_np_array/255
-        #converts to 4D tensor containing 1 image so that it works as an input to be predicted by the model as the model requires a 4d tensor for some reason
-#        tensor_4D = np.expand_dims(image_rescale, axis=0)
-#        prediction = CNN_model.predict(tensor_4D)
-#        print(f"prediction: {prediction}")
-#        print(f"type(prediction): {type(prediction)}")
-#        pred = round(prediction, 2)
-#        if abs(i - pred) > 45:
-#            false_pred = True
-#        else:
-#            false_pred = False
-#        pred_list.append((i, pred, false_pred, item))
-#        print(f"file: {item}, prediction: {prediction}")
-    
-
-os.chdir(initial_dir)
-
-def false_pred(actual_label, predicted_label):
-    if abs(actual_label - predicted_label) < 0.45:
-        return True
-    else:
-        return False
-
-def get_label(file_name):
-    label_1_or_0 = int(dirname(file_name)[-1])
-    return label_1_or_0
-
-log_file_name = f"{model_name[:-3]}_false_iden_log.csv"
+log_file_name = false_iden_log.csv
 with open(log_file_name, "a") as csv_logfile:
     csv_logfile_writer = csv.writer(csv_logfile, delimiter=",")
-    csv_logfile_writer.writerow(["Label", "Prediction", "False Prediction", "File Name"])
-    for index, pred in enumerate(prediction_list):
-        csv_logfile_writer.writerow([get_label(image_paths[index]), round(prediction_list[index][0],2), false_pred(get_label(image_paths[index]), round(prediction_list[index][0],2)), image_paths[index]])
-
-#pred_distribution_list = []
-#for j in pred_list:
-#    pred_distribution_list.append(i[1])
-
-#need as input for hist function to be list of all pred values in pred_list
-fig = plt.hist(prediction_list, bins=100)
-plt.title("Prediction_distribution")
-plt.xlabel("Prediction")
-plt.ylabel("No. of predictions")
-plt.savefig(f"{model_name}_pred_dist.png")
-
-end_time = time.perf_counter()
-print(f"time elapsed: {end_time - start_time}")
+    for item in false_list:
+        csv_logfile_writer.writerow([item])

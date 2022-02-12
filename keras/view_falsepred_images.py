044132a5fcead71c2aff8bbe91d23fa35acf00af
fiachraf <fiachraf12@gmail.com>
2022-02-12 23:58:58 +0000

    script to look at falsely classified images

---
+++ keras/view_falsepred_images.py
@@ -0,0 +1,19 @@
+import csv
+import matlpotlib.pyplot as plt
+import matplotlib.image as mpimg
+
+
+pred_csv = input("csv file containing predictions: ")
+
+with open(pred_csv) as csv_file:
+    csv_reader = csv.reader(csv_file)
+    row_index = 0
+    for row in csv_reader:
+        #row[0] is prediction, row[1] is label, row[2] is false prediction, row[3] is the file name
+        #row[2] might be different depending on file
+        #for the first one false means a false prediction and true means a true prediction
+        if row[3] == "False":
+            img = mpimg.imread(row[3])
+            plt.imshow(img)
+            plt.xlabel(f"Prediction: {row[0]}, label: {row[1]}, file name: {row[3]}")
+            plt.show()
